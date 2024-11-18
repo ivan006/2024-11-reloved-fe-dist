0f711cf1b43be19450276ca652ce6648aacd992f
Ivan Copeland <ivan.copeland2015@gmail.com>
2024-11-18 11:43:48 +0200

    1

--- js/qcalendar.js
+++ js/qcalendar.js
@@ -1,1 +1,1 @@
-import{w as o,P as s}from"./vendor.js";const e=o(({app:a})=>{a.use(s)});export{e as default};
+import{p as o,P as s}from"./vendor.js";const e=o(({app:a})=>{a.use(s)});export{e as default};
